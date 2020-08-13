d661338c583eef37adb65ec1b37c2487e285fe35
admin <admin>
2020-08-13 22:42:33 +0200

    Edited file 'webapps/wordcloud/backend.py'

--- webapps/wordcloud/backend.py
+++ webapps/wordcloud/backend.py
@@ -34,6 +34,15 @@ def color_func(word, font_size, position, orientation, random_state=None,
                   'hsl(120,57%,40%)', 'hsl(110,57%,71%)']
     return random.choice(color_list)
 
+def wordcloud_svg(text, colour_func):
+    # Generate a word cloud image
+    wordcloud = WordCloud(background_color='white', scale=2, margin=4, max_words=100)\
+    .generate(text).recolor(color_func=color_func, random_state=3)
+    
+    svg = wordcloud.to_svg(embed_font=True)
+    
+    return svg
+
 #Load text
 #text = str(df[text_col])
 
@@ -53,36 +62,47 @@ def color_func(word, font_size, position, orientation, random_state=None,
 
 @app.route('/get_svg/<path:params>')
 def get_svg(params):
+
     params_dict = json.loads(params)
     logging.info('Webapp parameters loaded: {}'.format(params_dict))
-    config = get_webapp_config().get("webAppConfig")
-    logging.info('Webapp config loaded: {}'.format(config))
-
-    logging.info('1')
 
     dataset_name = params_dict.get('dataset_name')
     df = dataiku.Dataset(dataset_name).get_dataframe()
 
-    logging.info('2')
+    text_col = params_dict.get('text_column', None)
+    language = params_dict.get('language', None)
+    facet_col = params_dict.get('facet_column', None)
 
-    text_col = params_dict.get('text_column')
-    language = params_dict.get('language')
-    facet_col = params_dict.get('facet_column')
+    if facet_col == None:
 
-    logging.info('3')
+        text = df[text_col].str.cat(sep=' ')
+        svg = wordcloud_svg(text, color_func)
+        logging.info('Wordcloud generated')
+        response = [{'facet': None, 'svg':chart_svg}]
+        return json.dumps(response)
 
-    text = str(df[text_col])
+    else:
 
-    logging.info('4')
+        facets = df[facet_col].unique().tolist()
+        texts = [df_grouped.get_group(facet)[text_col].str.cat(sep=' ') for facet in facets]
+        svgs = [wordcloud_svg(text, color_func) for text in texts]
+        logging.info('Wordclouds generated')
+        response = [{'facet':facet, 'svg':svg} for facet, svg in zip(facets, svgs)]
+        return json.dumps(response)
+
+
+    #text = str(df[text_col])
+
+    #logging.info('4')
 
     # Generate a word cloud image
-    print('Starting to generate wordcloud')
-    logging.info('Starting to generate wordcloud')
-    wordcloud = WordCloud(background_color='white', scale = 2 , margin=4, max_words=100)\
-    .generate(text).recolor(color_func=color_func, random_state=3)
+    #print('Starting to generate wordcloud')
+    #logging.info('Starting to generate wordcloud')
+    #wordcloud = WordCloud(background_color='white', scale = 2 , margin=4, max_words=100)\
+    #.generate(text).recolor(color_func=color_func, random_state=3)
 
-    chart_svg = wordcloud.to_svg(embed_font=True)
-    print('Wordcloud generated')
-    logging.info('Wordcloud generated')
-    return json.dumps({'chart': 'wordcloud', 'svg':chart_svg})
+    #chart_svg = wordcloud.to_svg(embed_font=True)
+    #print('Wordcloud generated')
+    #logging.info('Wordcloud generated')
+    #return json.dumps({'chart': 'wordcloud', 'svg':chart_svg})
 
