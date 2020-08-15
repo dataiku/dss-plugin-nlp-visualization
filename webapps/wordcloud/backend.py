4078194d0dd60a4b17bf85b2bd2451ac2a3508ff
admin <admin>
2020-08-15 19:43:18 +0200

    Edited file 'webapps/wordcloud/backend.py'

--- webapps/wordcloud/backend.py
+++ webapps/wordcloud/backend.py
@@ -31,50 +31,41 @@ def get_wordcloud_svg(text, colour_func):
 
 @app.route('/get_svg/<path:params>')
 def get_svg(params):
-    # Load parameters
-    params_dict = json.loads(params)
-    logging.info('Webapp parameters loaded: {}'.format(params_dict))
+    try:
+        # Load parameters
+        params_dict = json.loads(params)
+        logging.info('Webapp parameters loaded: {}'.format(params_dict))
 
-    dataset_name = params_dict.get('dataset_name', None)
-    text_col = params_dict.get('text_column', None)
-    language = params_dict.get('language', None)
-    subchart_column = params_dict.get('subchart_column', None)
+        dataset_name = params_dict.get('dataset_name', None)
+        text_col = params_dict.get('text_column', None)
+        language = params_dict.get('language', None)
+        subchart_column = params_dict.get('subchart_column', None)
 
-    # Load input dataframe
-    df = dataiku.Dataset(dataset_name).get_dataframe(columns=[text_col])
-    if df.empty:
-        raise Exception("Dataframe is empty")
+        # Load input dataframe
+        df = dataiku.Dataset(dataset_name).get_dataframe(columns=[text_col])
+        if df.empty:
+            raise Exception("Dataframe is empty")
 
-    if subchart_column == None:
-        text = df[text_col].str.cat(sep=' ')
-        svg = get_wordcloud_svg(text, color_func)
-        logging.info('Wordcloud generated')
-        response = [{'subchart': None, 'svg':svg}]
-        return json.dumps(response)
+        if subchart_column == None:
+            text = df[text_col].str.cat(sep=' ')
+            svg = get_wordcloud_svg(text, color_func)
+            logging.info('Wordcloud generated')
 
-    else:
+            response = [{'subchart': None, 'svg':svg}]
+            return json.dumps(response)
+        else:
+            df_grouped = df.groupby(subchart_column)
+            subcharts = df[subchart_column].unique().tolist()
 
-        df_grouped = df.groupby(subchart_column)
-        subcharts = df[subchart_column].unique().tolist()
-        texts = [df_grouped.get_group(subchart)[text_col].str.cat(sep=' ') for subchart in subcharts]
-        svgs = [get_wordcloud_svg(text, color_func) for text in texts]
-        logging.info('Wordclouds generated')
-        response = [{'subchart':subchart, 'svg':svg} for subchart, svg in zip(subcharts, svgs)]
-        return json.dumps(response)
+            texts = [df_grouped.get_group(subchart)[text_col].str.cat(sep=' ') for subchart in subcharts]
+            svgs = [get_wordcloud_svg(text, color_func) for text in texts]
+            logging.info('Wordclouds generated')
 
+            response = [{'subchart':subchart, 'svg':svg} for subchart, svg in zip(subcharts, svgs)]
+            return json.dumps(response)
+    
+    except:
+        logger.error(traceback.format_exc())
+        return traceback.format_exc(), 500
 
-    #text = str(df[text_col])
-
-    #logging.info('4')
-
-    # Generate a word cloud image
-    #print('Starting to generate wordcloud')
-    #logging.info('Starting to generate wordcloud')
-    #wordcloud = WordCloud(background_color='white', scale = 2 , margin=4, max_words=100)\
-    #.generate(text).recolor(color_func=color_func, random_state=3)
-
-    #chart_svg = wordcloud.to_svg(embed_font=True)
-    #print('Wordcloud generated')
-    #logging.info('Wordcloud generated')
-    #return json.dumps({'chart': 'wordcloud', 'svg':chart_svg})
 
