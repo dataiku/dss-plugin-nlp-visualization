5620cb0cd4b183c1efc0456c5f01fd4a254faedc
admin <admin>
2020-08-15 20:17:47 +0200

    Edited file 'webapps/wordcloud/backend.py'

--- webapps/wordcloud/backend.py
+++ webapps/wordcloud/backend.py
@@ -43,7 +43,7 @@ def get_svg(params):
 
         # Load input dataframe
         necessary_columns = [column for column in [text_column, subchart_column] if column != None]
-        df = dataiku.Dataset(dataset_name).get_dataframe(columns=[necessary_columns])
+        df = dataiku.Dataset(dataset_name).get_dataframe(columns=necessary_columns)
         if df.empty:
             raise Exception("Dataframe is empty")
 
