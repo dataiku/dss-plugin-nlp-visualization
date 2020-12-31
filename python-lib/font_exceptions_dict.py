a15212a7ccce90d8b6b182fe2d58c6d1a3446c82
admin <alexandre.landeau@essec.edu>
2020-12-31 13:08:59 +0100

    switched font for multilingual case

--- python-lib/font_exceptions_dict.py
+++ python-lib/font_exceptions_dict.py
@@ -7,28 +7,6 @@ FONT_EXCEPTIONS_DICT = {
     "ml": "NotoSansMerged-Regular-2048upem.ttf",
     "te": "NotoSansMerged-Regular-2048upem.ttf",
     "zh": "NotoSansCJKsc-Regular.otf",
-    "language_column": "NotoSansMerged-Regular-1000upem.ttf",
-}
-
-FONT_EXCEPTIONS_DICT2 = {
-    "ar": "NotoSansArabic-Regular.ttf",
-    "bn": "NotoSansBengali-Regular.ttf",
-    "fa": "NotoSansArabic-Regular.ttf",
-    "gu": "NotoSansGujarati-Regular.ttf",
-    "he": "NotoSansHebrew-Regular.ttf",
-    "hi": "NotoSansDevanagari-Regular.ttf",
-    "hy": "NotoSansArmenian-Regular.ttf",
-    "kn": "NotoSansKannada-Regular.ttf",
-    "ml": "NotoSansMalayalam-Regular.ttf",
-    "mr": "NotoSansDevanagari-Regular.ttf",
-    "ne": "NotoSansDevanagari-Regular.ttf",
-    "sa": "NotoSansDevanagari-Regular.ttf",
-    "si": "NotoSansSinhala-Regular.ttf",
-    "ta": "NotoSansTamil-Regular.ttf",
-    "te": "NotoSansTelugu-Regular.ttf",
-    "th": "NotoSansThai-Regular.ttf",
-    "ur": "NotoSansArabic-Regular.ttf",
-    "zh": "NotoSansCJKsc-Regular.otf",
     "language_column": "Arial Unicode.ttf",
 }
 
